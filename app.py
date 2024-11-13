5c20c2a56600157f5b9fb01ec214e91c7045ef6a
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-11-13 16:40:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,82 +3,118 @@ import streamlit as st
 import base64
 from openai import OpenAI
 
-# Configuración de página con tipografía y título centrado
-st.set_page_config(page_title="Análisis de Imagen", layout="centered", initial_sidebar_state="collapsed")
-st.markdown("<style>body { font-family: 'Lexend', sans-serif; }</style>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; font-family: Lexend, sans-serif;'>Análisis de Imagen 🤖🏞️</h1>", unsafe_allow_html=True)
-
-# Función para codificar imagen en base64
+# Function to encode the image to base64
 def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
-# Entrada para clave API
+
+st.set_page_config(page_title="Analisis dde imagen", layout="centered", initial_sidebar_state="collapsed")
+# Streamlit page setup
+st.title("Análisis de Imagen:🤖🏞️")
+#st.write(openai.Model.list())
 ke = st.text_input('Ingresa tu Clave')
+#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
+
+
+# Retrieve the OpenAI API Key from secrets
 api_key = os.environ['OPENAI_API_KEY']
 
-# Inicializar el cliente OpenAI con la clave
+# Initialize the OpenAI client with the API key
 client = OpenAI(api_key=api_key)
 
-# Carga de archivo de imagen
-uploaded_file = st.file_uploader("Sube una imagen", type=["jpg", "png", "jpeg"])
+# File uploader allows user to add their own image
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
-    # Mostrar imagen cargada con expandir
-    with st.expander("Imagen", expanded=True):
+    # Display the uploaded image
+    with st.expander("Image", expanded = True):
         st.image(uploaded_file, caption=uploaded_file.name, use_column_width=True)
 
-# Añadir toggle para detalles adicionales
-show_details = st.checkbox("Añadir detalles sobre la imagen", value=False)
+# Toggle for showing additional details input
+show_details = st.toggle("Adiciona detalles sobre la imagen", value=False)
 
 if show_details:
-    # Campo de texto para detalles adicionales
-    additional_details = st.text_area("Añadir contexto de la imagen aquí:")
+    # Text input for additional details about the image, shown only if toggle is True
+    additional_details = st.text_area(
+        "Adiciona contexto de la imagen aqui:",
+        disabled=not show_details
+    )
 
-# Botón para análisis de imagen
-analyze_button = st.button("Analizar la imagen")
+# Button to trigger the analysis
+analyze_button = st.button("Analiza la imagen", type="secondary")
 
-# Verificación de entrada para análisis
+# Check if an image has been uploaded, if the API key is available, and if the button has been pressed
 if uploaded_file is not None and api_key and analyze_button:
+
     with st.spinner("Analizando ..."):
+        # Encode the image
         base64_image = encode_image(uploaded_file)
+    
+        # Optimized prompt for additional clarity and detail
+        #prompt_text = (
+        #    "You are a highly knowledgeable scientific image analysis expert. "
+        #   "Your task is to examine the following image in detail. "
+        #    "Provide a comprehensive, factual, and scientifically accurate explanation of what the image depicts. "
+        #    "Highlight key elements and their significance, and present your analysis in clear, well-structured markdown format. "
+        #    "If applicable, include any relevant scientific terminology to enhance the explanation. "
+        #    "Assume the reader has a basic understanding of scientific concepts."
+        #    "Create a detailed image caption in bold explaining in short."
+        #    "The data is about electrical energy consumption and demand."
+        #    "Write when occurs the major and minor consumption, date and hour when this be possible."
+        #    "Explain always in spanish."
+        #)
 
-        # Prompt optimizado en español
-        prompt_text = (
-            "Eres un experto en análisis de imágenes científicas. Examina en detalle la siguiente imagen "
-            "y proporciona una explicación precisa de lo que representa. Destaca los elementos clave y su importancia, "
-            "en un formato claro y bien estructurado en markdown. Incluye terminología científica relevante en español."
-        )
-
+        prompt_text = ("Describe what you see in the image in spanish")
+    
         if show_details and additional_details:
-            prompt_text += f"\n\nContexto adicional proporcionado por el usuario:\n{additional_details}"
-
-        # Crear el mensaje para la API
+            prompt_text += (
+                f"\n\nAdditional Context Provided by the User:\n{additional_details}"
+            )
+    
+        # Create the payload for the completion request
         messages = [
             {
                 "role": "user",
                 "content": [
                     {"type": "text", "text": prompt_text},
-                    {"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64_image}"},
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
                 ],
             }
         ]
-
-        # Solicitud a la API de OpenAI
+    
+        # Make the request to the OpenAI API
         try:
+            # Without Stream
+            
+            # response = client.chat.completions.create(
+            #     model="gpt-4-vision-preview", messages=messages, max_tokens=500, stream=False
+            # )
+    
+            # Stream the response
             full_response = ""
             message_placeholder = st.empty()
             for completion in client.chat.completions.create(
-                model="gpt-4-vision-preview", messages=messages, max_tokens=1200, stream=True
+                model="gpt-4-vision-preview", messages=messages,   
+                max_tokens=1200, stream=True
             ):
+                # Check if there is content to display
                 if completion.choices[0].delta.content is not None:
                     full_response += completion.choices[0].delta.content
                     message_placeholder.markdown(full_response + "▌")
+            # Final update to placeholder after the stream ends
             message_placeholder.markdown(full_response)
+    
+            # Display the response in the app
+            # st.write(response.choices[0].message.content)
         except Exception as e:
-            st.error(f"Ocurrió un error: {e}")
+            st.error(f"An error occurred: {e}")
 else:
+    # Warnings for user action required
     if not uploaded_file and analyze_button:
-        st.warning("Por favor sube una imagen.")
+        st.warning("Please upload an image.")
     if not api_key:
         st.warning("Por favor ingresa tu API key.")
